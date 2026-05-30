59647547ec10cc620a9672d056ed2b1d2b496780
Jeffrey M. <therealjefftastic@gmail.com>
2026-05-30 01:25:33 -0400

    fix: item display overlap

--- src/main/java/com/github/jeffyjamzhd/renewed/mixins/gui/GuiIngameMixin.java
+++ src/main/java/com/github/jeffyjamzhd/renewed/mixins/gui/GuiIngameMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiIngame.class)
 @Environment(EnvType.CLIENT)
@@ -25,12 +25,21 @@ public class GuiIngameMixin {
         return this.mc.thePlayer.getMaxHealth();
     }
 
+    @Inject(method = "renderGameOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraft/PlayerControllerMP;shouldDrawHUD()Z", ordinal = 2))
+    private void moveItemDisplayIfHasLotsOfHeartsOrHunger(float par1, boolean par2, int par3, int par4, CallbackInfo ci, @Local(ordinal = 14) LocalIntRef y) {
+        float health = this.mc.thePlayer.getMaxHealth();
+        int nutrition = this.mc.thePlayer.getNutritionLimit();
+        if ((health > 10F || nutrition > 10) && this.mc.playerController.shouldDrawHUD()) {
+            y.set(y.get() - 14);
+        }
+    }
+
     @ModifyConstant(method = "func_110327_a", constant = @Constant(intValue = 10),
             slice = @Slice(
                     from = @At(value = "INVOKE", target = "Lnet/minecraft/Profiler;endStartSection(Ljava/lang/String;)V", ordinal = 1),
                     to = @At(value = "INVOKE", target = "Lnet/minecraft/Profiler;endStartSection(Ljava/lang/String;)V", ordinal = 2)
             ) )
-    private int drawFoodActualAmount(int constant) {
+    private int drawActualFoodAmount(int constant) {
         return this.mc.thePlayer.getNutritionLimit();
     }
 
@@ -48,7 +57,7 @@ public class GuiIngameMixin {
                     from = @At(value = "INVOKE", target = "Lnet/minecraft/Profiler;endStartSection(Ljava/lang/String;)V", ordinal = 1),
                     to = @At(value = "INVOKE", target = "Lnet/minecraft/Profiler;endStartSection(Ljava/lang/String;)V", ordinal = 2)
             ))
-    private int wrapHungerHorizontal(int drawX, @Local(ordinal = 13) int iteration, @Local(ordinal = 16) int x, @Local(ordinal = 6) int right) {
+    private int wrapHungerHorizontal(int _x, @Local(ordinal = 13) int iteration, @Local(ordinal = 6) int right) {
         return right - (iteration % 10) * 8 - 9;
     }
 }
