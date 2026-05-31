e06173c3a35b9d496b6a216b10a5c3e3d9754a20
Jeffrey M. <therealjefftastic@gmail.com>
2026-05-31 18:32:55 -0400

    feat: force mob drops toggle implemented

---
+++ src/main/java/com/github/jeffyjamzhd/renewed/mixins/difficulty/entity/EntityLivingBaseMixin.java
@@ -0,0 +1,25 @@
+package com.github.jeffyjamzhd.renewed.mixins.difficulty.entity;
+
+import com.github.jeffyjamzhd.renewed.api.difficulty.Difficulty;
+import com.github.jeffyjamzhd.renewed.registry.RenewedDifficulties;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.Entity;
+import net.minecraft.EntityLivingBase;
+import net.minecraft.World;
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(EntityLivingBase.class)
+public abstract class EntityLivingBaseMixin extends Entity {
+    public EntityLivingBaseMixin(World par1World) {
+        super(par1World);
+    }
+
+    @ModifyExpressionValue(method = "onDeath", at = @At(value = "FIELD", target = "Lnet/minecraft/EntityLivingBase;has_taken_massive_fall_damage:Z", opcode = Opcodes.GETFIELD))
+    private boolean forceDropsOnFallDamage(boolean original) {
+        Difficulty difficulty = Difficulty.getFromWorld(this.getWorld()).orElseThrow();
+        boolean dropLoot = difficulty.getParamValue(RenewedDifficulties.ANIMALS_ALWAYS_DROP_LOOT);
+        return !dropLoot && original;
+    }
+}
