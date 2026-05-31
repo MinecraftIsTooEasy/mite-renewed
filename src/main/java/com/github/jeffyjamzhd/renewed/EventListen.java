4455dae3ffe2998c38f573fe2339385f6108e2c7
Jeffrey M. <therealjefftastic@gmail.com>
2026-05-31 17:27:52 -0400

    chore: remove old difficulty packet method and class

--- src/main/java/com/github/jeffyjamzhd/renewed/EventListen.java
+++ src/main/java/com/github/jeffyjamzhd/renewed/EventListen.java
@@ -1,17 +1,12 @@
 package com.github.jeffyjamzhd.renewed;
 
-import com.github.jeffyjamzhd.renewed.api.IWorldInfo;
-import com.github.jeffyjamzhd.renewed.api.difficulty.Difficulty;
 import com.github.jeffyjamzhd.renewed.api.event.CraftingSoundRegisterEvent;
 import com.github.jeffyjamzhd.renewed.api.event.HandpanRegisterEvent;
 import com.github.jeffyjamzhd.renewed.api.event.MusicConditionRegisterEvent;
 import com.github.jeffyjamzhd.renewed.command.CommandTransform;
-import com.github.jeffyjamzhd.renewed.network.S2CSyncDifficulty;
 import com.github.jeffyjamzhd.renewed.registry.*;
 import com.google.common.eventbus.Subscribe;
 import moddedmite.rustedironcore.api.event.Handlers;
-import moddedmite.rustedironcore.network.Network;
-import net.minecraft.WorldServer;
 import net.xiaoyu233.fml.reload.event.*;
 
 public class EventListen extends Handlers {
@@ -48,9 +43,6 @@ public class EventListen extends Handlers {
 
     @Subscribe
     public void onPlayerLoggedIn(PlayerLoggedInEvent event) {
-        WorldServer world = event.getPlayer().getWorldServer();
-        Difficulty difficulty = ((IWorldInfo)world.getWorldInfo()).mr$getDifficulty();
-        Network.sendToClient(event.getPlayer(), new S2CSyncDifficulty(difficulty));
     }
 
     @Subscribe
