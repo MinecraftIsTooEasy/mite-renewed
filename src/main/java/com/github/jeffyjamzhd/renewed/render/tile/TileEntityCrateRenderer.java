88b2db239f8066ac3883441994a62f30c66fc462
Jeffrey M. <therealjefftastic@gmail.com>
2026-06-08 15:53:11 -0400

    fix: nasty gl state leak with enchanted items

--- src/main/java/com/github/jeffyjamzhd/renewed/render/tile/TileEntityCrateRenderer.java
+++ src/main/java/com/github/jeffyjamzhd/renewed/render/tile/TileEntityCrateRenderer.java
@@ -27,9 +27,10 @@ public class TileEntityCrateRenderer extends TileEntitySpecialRenderer {
                 renderGlint(x, y, 16, 16);
                 GL11.glDepthMask(true);
                 GL11.glDisable(GL11.GL_BLEND);
-                GL11.glDisable(GL11.GL_ALPHA_TEST);
                 GL11.glEnable(GL11.GL_LIGHTING);
                 GL11.glDepthFunc(GL11.GL_LEQUAL);
+                GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+                GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
             }
         }
 
