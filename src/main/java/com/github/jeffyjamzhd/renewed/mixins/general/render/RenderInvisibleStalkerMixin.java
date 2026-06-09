07df7898ebd2de83d2367e6ceb7400159ddfe97e
Jeffrey M. <therealjefftastic@gmail.com>
2026-06-09 14:54:44 -0400

    feat: update wight rendering

--- src/main/java/com/github/jeffyjamzhd/renewed/mixins/general/render/RenderInvisibleStalkerMixin.java
+++ src/main/java/com/github/jeffyjamzhd/renewed/mixins/general/render/RenderInvisibleStalkerMixin.java
@@ -7,8 +7,6 @@ import net.fabricmc.api.Environment;
 import net.minecraft.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Environment(EnvType.CLIENT)
 @Mixin(RenderInvisibleStalker.class)
@@ -17,18 +15,10 @@ public abstract class RenderInvisibleStalkerMixin extends RenderBiped {
         super(par1ModelBiped, par2);
     }
 
-    @Inject(method = "setTextures", at = @At("HEAD"), cancellable = true)
-    private void setTextureWithEmission(CallbackInfo ci) {
-        this.setTexture(0, "textures/entity/wight");
-        this.textures_glowing[0] = new ResourceLocation("textures/entity/shadow_glow.png");
-        ci.cancel();
-    }
-
     @ModifyReturnValue(method = "getModelOpacity", at = @At("RETURN"))
     private float setModelOpacity(float original, @Local(argsOnly = true) Entity entity) {
         EntityPlayer player = Minecraft.getClientPlayer();
         double dist = player.getDistanceSqToEntity(entity);
-        float opacity = (float) Math.max(0.05F, Math.min(0.5F, (50 - dist) / 100));
-        return opacity;
+        return (float) Math.max(0.01F, Math.min(0.5F, (50 - dist) / 100));
     }
 }
