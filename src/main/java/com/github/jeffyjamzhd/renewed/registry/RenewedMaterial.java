c7adc1e4f46320e07ec2c6f512ab5a302594dbbd
Jeffrey M. <therealjefftastic@gmail.com>
2026-06-16 13:04:20 -0400

    fix: bone enchantability and quality

--- src/main/java/com/github/jeffyjamzhd/renewed/registry/RenewedMaterial.java
+++ src/main/java/com/github/jeffyjamzhd/renewed/registry/RenewedMaterial.java
@@ -1,8 +1,12 @@
 package com.github.jeffyjamzhd.renewed.registry;
 
 import com.github.jeffyjamzhd.renewed.item.material.MaterialBone;
+import net.minecraft.EnumQuality;
 import net.minecraft.Material;
 
 public class RenewedMaterial {
-    public static final Material bone = new MaterialBone().setMinHarvestLevel(2);
+    public static final Material bone = new MaterialBone()
+            .setMinHarvestLevel(2)
+            .setEnchantability(0)
+            .setMaxQuality(EnumQuality.average);
 }
